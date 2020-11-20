134263b28163235bd60fbbf96e7eef43c998f8c3
Ben8842 <z87e99b44r55a@gmail.com>
2020-11-19 20:45:41 -0500

    first

--- src/App.js
+++ src/App.js
@@ -35,6 +35,8 @@ class App extends Component {
     super(props);
     this.state = {
       address: "",
+      isLoaded: false,
+      citems: [],
     };
     this.closeModal = this.closeModal.bind(this);
     this.onInputChange = this.onInputChange.bind(this);
@@ -43,6 +45,17 @@ class App extends Component {
     this.openDeleteModal = this.openDeleteModal.bind(this);
     this.openPromptModal = this.openPromptModal.bind(this);
     this.showInput = this.showInput.bind(this);
+  }
+
+  componentDidMount() {
+    fetch("https://api.covidtracking.com/v1/states/current.json")
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+          isLoaded: true,
+          citems: json,
+        });
+      });
   }
 
   closeModal() {
@@ -82,11 +95,16 @@ class App extends Component {
   }
 
   openConfirmModal() {
+    var { isLoaded, citems } = this.state;
+
     this.props.showModal(
       {
         open: true,
-        title: "Confirm Modal",
-        message: MESSAGE,
+        title: "Texas",
+        message: "Texas has " + citems[47].positive + " positive cases",
+        message2:
+          citems[47].hospitalizedCurrently + " are currently hospitalized",
+        message3: "Texas has " + citems[47].death + " total deaths",
         confirmAction: this.closeModal,
         closeModal: this.closeModal,
       },
@@ -128,54 +146,72 @@ class App extends Component {
   }
 
   render() {
-    return (
-      <div className="app">
-        <USAMap customize={this.statesFilling()} onClick={this.mapHandler} />
-        <header className="app-header">
-          <h1 className="app-title">COVID</h1>
-        </header>
-        <div className="container">
-          <div className="modal-types row d-flex justify-content-center align-items-center">
-            <div className="col">
-              <button
-                className="btn btn-outline-primary btn-block"
-                onClick={this.openAlertModal}
-              >
-                alert
-              </button>
-            </div>
-            <div className="col">
-              <button
-                className="btn btn-outline-primary btn-block"
-                onClick={this.openConfirmModal}
-              >
-                confirm
-              </button>
-            </div>
-            <div className="col">
-              <button
-                className="btn btn-outline-primary btn-block"
-                onClick={this.openDeleteModal}
-              >
-                delete
-              </button>
-            </div>
-            <div className="col">
-              <button
-                className="btn btn-outline-primary btn-block"
-                onClick={this.openPromptModal}
-              >
-                prompt
-              </button>
+    var { isLoaded, citems } = this.state;
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="app">
+          <h1 className="app-titles">COVID-19</h1>
+          <h1 className="app-titles">Click on a State</h1>
+          <USAMap customize={this.statesFilling()} onClick={this.mapHandler} />
+
+          <header className="app-header">
+            <h1 className="app-title">COVID-19</h1>
+          </header>
+          <div className="container">
+            <div className="modal-types row d-flex justify-content-center align-items-center">
+              <div className="col">
+                <button
+                  className="btn btn-outline-primary btn-block"
+                  onClick={this.openAlertModal}
+                >
+                  alert
+                </button>
+              </div>
+              <div className="col">
+                <button
+                  className="btn btn-outline-primary btn-block"
+                  onClick={this.openConfirmModal}
+                >
+                  confirm
+                </button>
+              </div>
+              <div className="col">
+                <button
+                  className="btn btn-outline-primary btn-block"
+                  onClick={this.openDeleteModal}
+                >
+                  delete
+                </button>
+              </div>
+              <div className="col">
+                <button
+                  className="btn btn-outline-primary btn-block"
+                  onClick={this.openPromptModal}
+                >
+                  prompt
+                </button>
+              </div>
             </div>
           </div>
+          <ul>
+            DATA HAS BEEN LOADED
+            {citems.map((item) => (
+              <li key={item.id}>
+                Positive Cases: {item.positive} | State: {item.state} |{" "}
+                {item.id}
+              </li>
+            ))}
+            ;
+          </ul>
+          <span className="app-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </span>
+          <ModalRoot hideModal={this.props.hideModal} />
         </div>
-        <span className="app-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </span>
-        <ModalRoot hideModal={this.props.hideModal} />
-      </div>
-    );
+      );
+    }
   }
 }
 
--- src/components/Modals/ConfirmModal.js
+++ src/components/Modals/ConfirmModal.js
@@ -1,25 +1,49 @@
-import React from 'react'
+import React from "react";
 
-const ConfirmModal = ({ closeModal, confirmAction, title, message }) => {
+const ConfirmModal = ({
+  closeModal,
+  confirmAction,
+  title,
+  message,
+  message2,
+  message3,
+}) => {
   return (
     <div className="modal-content">
       <div className="modal-header">
-        <h5
-          className="modal-title"
-        >{title}</h5>
-        <button type="button" className="close" aria-label="Close" onClick={closeModal}>
+        <h5 className="modal-title">{title}</h5>
+        <button
+          type="button"
+          className="close"
+          aria-label="Close"
+          onClick={closeModal}
+        >
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div className="modal-body">
         <p>{message}</p>
+        <p>{message2}</p>
+        <p>{message3}</p>
       </div>
       <div className="modal-footer">
-        <button type="button" className="btn btn-secondary" onClick={closeModal}>Cancel</button>
-        <button type="button" className="btn btn-primary" onClick={confirmAction}>Continue</button>
+        <button
+          type="button"
+          className="btn btn-secondary"
+          onClick={closeModal}
+        >
+          Cancel
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={confirmAction}
+        >
+          Continue
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ConfirmModal
+export default ConfirmModal;
